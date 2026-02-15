4636ba5436f5a090488b677de2ec4ba2d93ac136
PandeyKunal <pandeykunal563@gmail.com>
2026-02-15 18:48:34 +0530

    okk

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,64 +5,53 @@ import userModel from '../models/userModel.js'
 // API Controller Function to manage Clerk user with database
 // http://localhost:4000/api/user/webhooks
 const clerkWebhooks = async (req, res) => {
-
     try {
 
-        // Create a Svix instance with clerk webhook secret.
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
-        // Verifying Headers
-        await whook.verify(JSON.stringify(req.body), {
+        const payload = whook.verify(req.body, {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"],
         })
 
-        // Getting Data from request body
-        const { data, type } = req.body
+        const { data, type } = payload
 
-        // Switch Cases for differernt Events
         switch (type) {
-            case 'user.created': {
-                const userData = {
+
+            case 'user.created':
+                await userModel.create({
                     clerkId: data.id,
                     email: data.email_addresses[0].email_address,
                     firstName: data.first_name,
                     lastName: data.last_name,
                     photo: data.image_url,
-                }
-                await userModel.create(userData)
-                res.json({})
+                })
                 break;
-            }
 
-            case 'user.updated': {
-                const userData = {
-                    email: data.email_addresses[0].email_address,
-                    firstName: data.first_name,
-                    lastName: data.last_name,
-                    photo: data.image_url,
-                }
-                await userModel.findOneAndUpdate({ clerkId: data.id }, userData)
-                res.json({})
+            case 'user.updated':
+                await userModel.findOneAndUpdate(
+                    { clerkId: data.id },
+                    {
+                        email: data.email_addresses[0].email_address,
+                        firstName: data.first_name,
+                        lastName: data.last_name,
+                        photo: data.image_url,
+                    }
+                )
                 break;
-            }
 
-            case 'user.deleted': {
+            case 'user.deleted':
                 await userModel.findOneAndDelete({ clerkId: data.id })
-                res.json({})
-                break;
-            }
-
-            default:
                 break;
         }
 
+        res.status(200).json({ success: true })
+
     } catch (error) {
         console.log(error.message)
-        res.json({ success: false, message: error.message })
+        res.status(400).json({ success: false })
     }
-
 }
 
 export {clerkWebhooks}
